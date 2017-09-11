578c80ba88c78c65489050d032c17b87c2723816
Yuriy Yurenko <yurenkoyura@gmail.com>
2017-09-11 16:46:27 +0300

    Merge branch 'ngorobets' into yyurenko

---
+++ custom/Extension/modules/bh_scripts/Ext/Vardefs/script_fields.php
@@ -0,0 +1,23 @@
+<?php
+$dictionary['bh_scripts']['fields']['script_status'] = array (
+         'required' => false,
+          'name' => 'script_status',
+          'vname' => 'LBL_SCRIPT_STATUS',
+          'type' => 'enum',
+          'massupdate' => 0,
+          'no_default' => false,
+          'comments' => '',
+          'help' => '',
+          'importable' => 'true',
+          'duplicate_merge' => 'disabled',
+          'duplicate_merge_dom_value' => '0',
+          'audited' => false,
+          'inline_edit' => true,
+          'reportable' => true,
+          'unified_search' => false,
+          'merge_filter' => 'disabled',
+          'options' => 'script_status_list',
+          'studio' => 'visible',
+          'size' => '20',
+          'enable_range_search' => false,
+    );
