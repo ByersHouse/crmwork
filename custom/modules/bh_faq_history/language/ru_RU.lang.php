fcad28dea9fd50f2c635f0d6b520078995312970
Yuriy Yurenko <yurenkoyura@gmail.com>
2017-09-12 15:54:11 +0300

    Добавление поля комментарий

--- custom/modules/bh_faq_history/language/ru_RU.lang.php
+++ custom/modules/bh_faq_history/language/ru_RU.lang.php
@@ -1,8 +1,9 @@
 <?php
-// created: 2017-09-12 11:53:51
+// created: 2017-09-12 15:51:18
 $mod_strings = array (
   'LBL_ASSIGNED_TO_NAME' => 'Пользователь',
   'LBL_DESCRIPTION' => 'Комментарий',
   'LBL_REDACT_CONTENT' => 'Редакция содержания',
   'LBL_DATE_REDACT' => 'Дата редакции',
+  'LBL_NAME' => 'Комментарий',
 );
