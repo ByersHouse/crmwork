86b53d18341b80a26b220c4da69b7b264946491c
Yuriy Yurenko <yurenkoyura@gmail.com>
2017-09-12 10:38:47 +0300

    добавил на форму

--- custom/modules/bh_faq_history/language/ru_RU.lang.php
+++ custom/modules/bh_faq_history/language/ru_RU.lang.php
@@ -1,5 +1,6 @@
 <?php
-// created: 2017-09-12 10:36:25
+// created: 2017-09-12 10:37:41
 $mod_strings = array (
   'LBL_ASSIGNED_TO_NAME' => 'Пользователь',
+  'LBL_DESCRIPTION' => 'Комментарий',
 );
