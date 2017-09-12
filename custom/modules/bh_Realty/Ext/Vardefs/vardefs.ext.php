640e82fea4d98baa47f7fb9a9596fd6814c630cf
nikolay <nikolay.strafe@gmail.com>
2017-09-12 11:58:13 +0300

    добавлено поле "Вид собственности" в недвижимости

---
+++ custom/modules/bh_Realty/Ext/Vardefs/vardefs.ext.php
@@ -0,0 +1,28 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+
+
+$dictionary['bh_Realty']['fields']['realty_typeOwnership'] = array (
+    'required' => false,
+    'name' => 'realty_typeOwnership',
+    'vname' => 'LBL_TYPE_OWNERSHIP',
+    'type' => 'enum',
+    'massupdate' => 0,
+    'no_default' => false,
+    'comments' => '',
+    'help' => '',
+    'importable' => 'true',
+    'duplicate_merge' => 'disabled',
+    'duplicate_merge_dom_value' => '0',
+    'audited' => false,
+    'inline_edit' => true,
+    'reportable' => true,
+    'unified_search' => false,
+    'merge_filter' => 'disabled',
+    'options' => 'realty_typeOwnership_list',
+    'studio' => 'visible',
+    'size' => '20',
+    'enable_range_search' => false,
+);
+?>
