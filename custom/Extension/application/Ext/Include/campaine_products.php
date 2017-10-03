dde36ddfde4ca30c7cae498942be101f43c10d46
Yuriy Yurenko <yurenkoyura@gmail.com>
2017-10-03 14:04:48 +0300

    создание установка модуля Продукты компании

---
+++ custom/Extension/application/Ext/Include/campaine_products.php
@@ -0,0 +1,7 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['bh_campaine_products'] = 'bh_campaine_products';
+$beanFiles['bh_campaine_products'] = 'modules/bh_campaine_products/bh_campaine_products.php';
+$moduleList[] = 'bh_campaine_products';
+
+?>
--- custom/application/Ext/Include/modules.ext.php
+++ custom/application/Ext/Include/modules.ext.php
@@ -10,6 +10,13 @@ $moduleList[] = 'bh_Autotransport';
 
  
  //WARNING: The contents of this file are auto-generated
+$beanList['bh_campaine_products'] = 'bh_campaine_products';
+$beanFiles['bh_campaine_products'] = 'modules/bh_campaine_products/bh_campaine_products.php';
+$moduleList[] = 'bh_campaine_products';
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
 $beanList['bh_Card_ident'] = 'bh_Card_ident';
 $beanFiles['bh_Card_ident'] = 'modules/bh_Card_ident/bh_Card_ident.php';
 $moduleList[] = 'bh_Card_ident';
