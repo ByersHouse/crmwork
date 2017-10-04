be64ff50bd939f54cad68a4e937d0c14f7b91ce9
Yuriy Yurenko <yurenkoyura@gmail.com>
2017-10-04 12:33:08 +0300

    добавление полей списка Продукты кампании

---
+++ custom/modules/bh_campaine_products/metadata/listviewdefs.php
@@ -0,0 +1,52 @@
+<?php
+$module_name = 'bh_campaine_products';
+$listViewDefs [$module_name] = 
+array (
+  'NAME' => 
+  array (
+    'width' => '32%',
+    'label' => 'LBL_NAME',
+    'default' => true,
+    'link' => true,
+  ),
+  'BH_MARKETING_COMPANY_BH_CAMPAINE_PRODUCTS_1_NAME' => 
+  array (
+    'type' => 'relate',
+    'link' => true,
+    'label' => 'LBL_BH_MARKETING_COMPANY_BH_CAMPAINE_PRODUCTS_1_FROM_BH_MARKETING_COMPANY_TITLE',
+    'id' => 'BH_MARKETI9ABCCOMPANY_IDA',
+    'width' => '10%',
+    'default' => true,
+  ),
+  'DATE_MODIFIED' => 
+  array (
+    'type' => 'datetime',
+    'label' => 'LBL_DATE_MODIFIED',
+    'width' => '10%',
+    'default' => true,
+  ),
+  'DATE_ENTERED' => 
+  array (
+    'type' => 'datetime',
+    'label' => 'LBL_DATE_ENTERED',
+    'width' => '10%',
+    'default' => true,
+  ),
+  'DESCRIPTION' => 
+  array (
+    'type' => 'text',
+    'label' => 'LBL_DESCRIPTION',
+    'sortable' => false,
+    'width' => '10%',
+    'default' => true,
+  ),
+  'ASSIGNED_USER_NAME' => 
+  array (
+    'width' => '9%',
+    'label' => 'LBL_ASSIGNED_TO_NAME',
+    'module' => 'Employees',
+    'id' => 'ASSIGNED_USER_ID',
+    'default' => true,
+  ),
+);
+?>
