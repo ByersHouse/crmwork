d7ea73618898ce9024d651157d978f82b638a0d3
Yuriy Yurenko <yurenkoyura@gmail.com>
2017-10-05 16:06:19 +0300

    коммент

--- custom/modules/bh_Card_ident/js/editview.js
+++ custom/modules/bh_Card_ident/js/editview.js
@@ -3,7 +3,7 @@
 
 
 $( document ).ready(function() {
- 
+    //только цифры в номере карты
     $('#name').bind("change keyup input click", function() {
         if (this.value.match(/[^0-9]/g)) {
             this.value = this.value.replace(/[^0-9]/g, '');
