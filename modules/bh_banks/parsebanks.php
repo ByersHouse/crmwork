30c43f61bc60638eb59a0bf11604195b40468b1e
Yuriy Yurenko <yurenkoyura@gmail.com>
2017-10-07 14:18:40 +0300

    допилил парсер

--- modules/bh_banks/parsebanks.php
+++ modules/bh_banks/parsebanks.php
@@ -1,7 +1,7 @@
 <?php
 
 header('Content-type:text/html; charset=utf-8');
-require_once 'phpQuery.php';
+require_once 'include\phpQuery\phpQuery.php';
 $link = mysql_connect('127.0.0.1', 'root', '');
 if (!$link) {
     
@@ -20,26 +20,41 @@ $urls = array(
         'https://bank.gov.ua/control/bankdict/banks?type=369&sort=name&cPage=3&startIndx=61',
         'https://bank.gov.ua/control/bankdict/banks?type=369&sort=name&cPage=4&startIndx=81'
 );
+echo "Начинаю загрузку с адреса ..";
 foreach ($urls as $url){
+    //
     $file = file_get_contents($url);
     $doc = phpQuery::newDocument($file);
     $tbl = $doc->find('.col_title_t');
     $i=0;
-    var_dump($tbl);  exit;
+ 
     foreach ($tbl->find('tr') as $items) {
         $items = pq($items);
         if($i!=0){
             $bank_name = trim($items->find('td')->eq(0)->text());
             $bank_edprou = trim($items->find('td')->eq(1)->text());
             $bank_mfo = trim($items->find('td')->eq(2)->text());
-            //$date_registr = trim($items->find('td')->eq(3)->text());
-            $bank_adress = trim($items->find('td')->eq(4)->text());
-            $sql = "INSERT INTO banks (name,edrpou,mfo,jur_address) 
-                         VALUES ('{$bank_name}','{$bank_edprou}','{$bank_mfo}','{$bank_adress}')";
-            mysql_query($sql,$link);
+            $sql = "SELECT id from bh_banks WHERE mfo ='{$bank_mfo}'";
+            $result = mysql_query($sql,$link);
+            $row = mysql_fetch_array($result, MYSQL_NUM);
+            // var_dump($row,"</br>");
+            if(!$row){
+                $id = create_guid();
+               
+                $bank_adress = trim($items->find('td')->eq(4)->text());
+                $sql = "INSERT INTO bh_banks (id,name,okpo,mfo,description) 
+                         VALUES ('{$id}','{$bank_name}','{$bank_edprou}','{$bank_mfo}','{$bank_adress}')";
+                         
+                         //var_dump($sql);
+                mysql_query($sql,$link);
+                echo "</br>банк <a href=\"index.php?action=ajaxui#ajaxUILoc=index.php%3Fmodule%3Dbh_banks%26offset%3D1%26stamp%3D1507373944011808000%26return_module%3Dbh_banks%26action%3DDetailView%26record%3D{$id}\">{$bank_name}</a> добавлен в базу данных</br>";
+            }
+          
+        
         }
         $i++;
     }
+   /// echo "всего {$i} ";
 sleep(3);
 }
 mysql_close($link);
