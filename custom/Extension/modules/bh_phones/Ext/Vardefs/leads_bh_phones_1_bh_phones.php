ea4b1df3137235348381404b8bc7525f63854e8a
Yuriy Yurenko <yurenkoyura@gmail.com>
2017-10-09 16:27:53 +0300

    Сделал поле обязательным

--- custom/Extension/modules/bh_phones/Ext/Vardefs/leads_bh_phones_1_bh_phones.php
+++ custom/Extension/modules/bh_phones/Ext/Vardefs/leads_bh_phones_1_bh_phones.php
@@ -33,6 +33,7 @@ $dictionary["bh_phones"]["fields"]["leads_bh_phones_1leads_ida"] = array (
   'relationship' => 'leads_bh_phones_1',
   'source' => 'non-db',
   'reportable' => false,
+  'required' => true, 
   'side' => 'right',
   'vname' => 'LBL_LEADS_BH_PHONES_1_FROM_BH_PHONES_TITLE',
 );
