1d3b4997c5a83e4e2726ec418e88bb81746c12a3
Yuriy Yurenko <yurenkoyura@gmail.com>
2017-10-09 16:35:56 +0300

    СДЕЛАЛ ПОЛЕ ПРЕДВАРРИТ КОНТАКТЫ ОБЯЗАТЕЛЬНЫМ ДЛЯ ТЕЛЕФОНОВ

--- custom/Extension/modules/bh_phones/Ext/Vardefs/leads_bh_phones_1_bh_phones.php
+++ custom/Extension/modules/bh_phones/Ext/Vardefs/leads_bh_phones_1_bh_phones.php
@@ -20,6 +20,7 @@ $dictionary["bh_phones"]["fields"]["leads_bh_phones_1_name"] = array (
   'link' => 'leads_bh_phones_1',
   'table' => 'leads',
   'module' => 'Leads',
+  'required' => true,   
   'rname' => 'name',
   'db_concat_fields' => 
   array (
@@ -33,7 +34,6 @@ $dictionary["bh_phones"]["fields"]["leads_bh_phones_1leads_ida"] = array (
   'relationship' => 'leads_bh_phones_1',
   'source' => 'non-db',
   'reportable' => false,
-  'required' => true, 
   'side' => 'right',
   'vname' => 'LBL_LEADS_BH_PHONES_1_FROM_BH_PHONES_TITLE',
 );
