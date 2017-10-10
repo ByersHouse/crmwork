59ba9c78f46daa9fe1c976c7788ec2a31d302dbb
Yuriy Yurenko <yurenkoyura@gmail.com>
2017-10-10 10:30:32 +0300

    подправил парсер банков

--- custom/modules/Leads/LogicHooks/SendAlert.php
+++ custom/modules/Leads/LogicHooks/SendAlert.php
@@ -7,10 +7,13 @@ class LeadHooks{
     public function sendAlert($bean,$events,$arguments){
         sleep(5);
         $lead = BeanFactory::getBean('Leads', $bean->fetched_row['id']);
+        
+       // var_dump("<pre>",$lead,"</pre>");
+     //  exit;
         if($lead->assigned_user_id){
             
             $seedAlert = new Alert();
-            $seedAlert->name = "Предварительный контакт '".$lead->first_name." ".$lead->last_name."'";
+            $seedAlert->name = "Предварительный контакт ".$lead->first_name." ".$lead->last_name."";
             $seedAlert->description = "Вам назначена задача";
             $seedAlert->assigned_user_id = $lead->assigned_user_id;
             $seedAlert->message_to_assigned = $lead->message_to_assigned;
